e7490f9cf89f74e5de5aebbc41a4be069eded0fa
Julien <julien.malaper@casinocosmopol.se>
2024-06-29 14:22:52 +0200

    Add flight model and controller with listing of flights with api route

---
+++ app/Http/Controllers/FlightController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Flight;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(){
+
+        $flights = Flight::select('id', 'source_airport', 'destination_airport')->get()->map(function($flight) {
+            return [
+                'id' => $flight->id,
+                'departure_time' => $flight->formatted_departure_time,
+                'source_airport' => $flight->source_airport,
+                'destination_airport' => $flight->destination_airport,
+            ];
+        });
+
+        return response()->json(['message' => 'Flights list retrieved successfully', 'flights' => $flights], 201);
+    }
+
+}
---
+++ app/Models/Flight.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * App\Models\Flight
+ *
+ * @property int $id
+ * @property string|null $source_airport
+ * @property string|null $destination_airport
+ * @property \Illuminate\Support\Carbon|null $departure_time
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereId($value)
+ * 
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereSourceAirport($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereDestinationAirport($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Flight whereDepartureTime($value)
+ */
+class Flight extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'departure_time',
+        'source_airport',
+        'destination_airport'
+    ];
+
+    protected $appends = ['formatted_departure_time'];
+
+    /**
+     * Method to get departure time in the following format: Y-m-d H:i
+     *
+     * @return Carbon
+     */
+    public function getFormattedDepartureTimeAttribute(){
+        return Carbon::parse($this->departure_time)->format('Y-m-d H:i');
+    }
+
+
+    /********************************************************************/
+    /**************************** RELATIONS *****************************/
+    /********************************************************************/
+
+    /**
+     * Get tickets of the flight
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasMany
+     */
+    public function tickets() {
+        return $this->hasMany(Ticket::class);
+    }
+}
