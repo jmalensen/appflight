7d3f24d2eb5b2bd5fe6231de352d5ead6cef6fab
Julien <julien.malaper@casinocosmopol.se>
2024-06-29 14:41:01 +0200

    Prepare methods for ticket controller and routes for API

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,14 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTicketRequest;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Method to create ticket
+     *
+     * @param CreateTicketRequest $request
+     * @return Response
      */
-    public function index(){
-        //
+    public function create(CreateTicketRequest $request){
+
     }
+
+
+    /**
+     * Method to cancel the ticket
+     *
+     * @param string $ticketNumber
+     * @return Response
+     */
+    public function cancel($ticketNumber){
+
+    }
+
+
+    /**
+     * Method to update the seat of the ticket
+     *
+     * @param Request $request
+     * @param string $ticketNumber
+     * @return Response
+     */
+    public function updateSeat(Request $request, $ticketNumber){
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FlightController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/flights', [FlightController::class, 'index']);
+
+Route::post('/tickets', [TicketController::class, 'create']);
+Route::delete('/tickets/{id}', [TicketController::class, 'cancel']);
+Route::put('/tickets/{ticket}/seat', [TicketController::class, 'updateSeat']);
