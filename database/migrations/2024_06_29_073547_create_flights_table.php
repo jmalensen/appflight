d587d044c5e17a00a706b0dcaae23821abd4cb61
Julien <julien.malaper@casinocosmopol.se>
2024-06-29 14:16:00 +0200

    Add migrations for flights, customers and tickets table

---
+++ database/migrations/2024_06_29_073547_create_flights_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('departure_time')->nullable();
+            $table->string('source_airport')->nullable();
+            $table->string('destination_airport')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
